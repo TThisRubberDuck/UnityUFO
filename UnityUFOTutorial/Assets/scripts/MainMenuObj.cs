8101022e904ddbcaab9a1b7f272b21d1416ee196
TThisRubberDuck <47885326+TThisRubberDuck@users.noreply.github.com>
2019-03-08 12:05:02 +0000

    completed tutorial and added spikes

---
+++ UnityUFOTutorial/Assets/scripts/MainMenuObj.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuObj : MonoBehaviour
+{
+  
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {  // Detects if any key has been pressed.
+            Debug.Log("A key or mouse click has been detected");
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
---
+++ UnityUFOTutorial/Assets/scripts/SpikeTrap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeTrap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("Spikes"))
+
+            //... then set the other object we just collided with to inactive.
+                 SceneManager.LoadScene("Main");
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+
+
+
+    }
+}
